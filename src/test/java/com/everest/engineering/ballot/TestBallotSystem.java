4d44a881f185d36665e70f8d69541c9382020155
Mohammad Nawazish Khan <md.nawazish.khan@gmail.com>
2019-02-20 11:04:33 +0530

    Fix casting of votes with the condition that campaigning kingdoms do not express their allegiance even if message send to them are correct

--- src/test/java/com/everest/engineering/ballot/TestBallotSystem.java
+++ src/test/java/com/everest/engineering/ballot/TestBallotSystem.java
@@ -1,9 +1,9 @@
 package com.everest.engineering.ballot;
 
-import com.everest.engineering.constants.StringConstants;
 import com.everest.engineering.factory.kingdom.KingdomFactory;
 import com.everest.engineering.factory.universe.UniverseFactory;
-import com.everest.engineering.message.Message;
+import com.everest.engineering.message.AbstractMessage;
+import com.everest.engineering.message.DefaultMessage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,9 +14,8 @@ import java.util.List;
 public class TestBallotSystem {
     @Test
     public void testRegisterCampainingKingdoms(){
-        class AbstractBallotSystemTestDouble extends AbstractBallotSystem{}
         List<String> expected = Arrays.asList("ICE", "LAND");
-        AbstractBallotSystem abstractBallotSystem = new AbstractBallotSystemTestDouble();
+        AbstractBallotSystem abstractBallotSystem = new DefaultBallotSystem();
         abstractBallotSystem.registerCampainingKingdoms(expected);
         List<String> actual = abstractBallotSystem.getCampainingKingdoms();
         Assertions.assertIterableEquals(expected, actual);
@@ -26,22 +25,21 @@ public class TestBallotSystem {
     public void testCasteVote(){
         List<String> kingdoms = UniverseFactory.getAllKingdoms();
         List<String> campaigningKingdoms = Arrays.asList("ICE", "LAND");
-        List<Message> expectedBallot = new ArrayList<>();
+        List<AbstractMessage> expectedBallot = new ArrayList<>();
 
         for (String campainingKingdom : campaigningKingdoms){
             for (String kingdom : kingdoms){
-                if (!campainingKingdom.equals(kingdom)) //Because campaign shouldn't send message to its own kingdom
+                if (!campaigningKingdoms.contains(kingdom)) //Because campaign shouldn't send message to its own kingdom
                     expectedBallot.add(
-                            new Message(KingdomFactory.getInstance(campainingKingdom),
+                            new DefaultMessage(KingdomFactory.getInstance(campainingKingdom),
                                     KingdomFactory.getInstance(kingdom.toUpperCase()))
                     );
             }
         }
-        class AbstractBallotSystemTestDouble extends AbstractBallotSystem{}
-        AbstractBallotSystem abstractBallotSystem = new AbstractBallotSystemTestDouble();
+        AbstractBallotSystem abstractBallotSystem = new DefaultBallotSystem();
         abstractBallotSystem.registerCampainingKingdoms(campaigningKingdoms);
         abstractBallotSystem.casteVote();
-        List<Message> actual = abstractBallotSystem.getBallot();
+        List<AbstractMessage> actual = abstractBallotSystem.getBallot();
 
         if (expectedBallot.size()!=actual.size())
             Assertions.assertFalse(true, "Ballot size failure");
