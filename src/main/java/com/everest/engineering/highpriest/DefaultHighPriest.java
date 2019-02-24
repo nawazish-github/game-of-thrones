ae72bb20c4448264338806a624ca11bba3d8099d
Mohammad Nawazish Khan <md.nawazish.khan@gmail.com>
2019-02-24 18:25:10 +0530

    Fix random selection of votes from ballot and selection of messages from the Message Table

--- src/main/java/com/everest/engineering/highpriest/DefaultHighPriest.java
+++ src/main/java/com/everest/engineering/highpriest/DefaultHighPriest.java
@@ -4,15 +4,19 @@ import com.everest.engineering.message.AbstractMessage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class DefaultHighPriest extends AbstractHighPriest {
     @Override
     public void luckyDrawMessages(List<AbstractMessage> ballot) {
+        Random random = new Random();
         int votesCount = ballot.size();
         List<AbstractMessage> list = new ArrayList<>();
         for (int i = 0; i < 6 ; i++){
-            int rand = (int)Math.random()%votesCount;
-            list.add(ballot.get(rand));
+            int r = random.nextInt();
+            int rand = (int)r%votesCount;
+            --votesCount; //the chosen vote should not be again chosen.
+            list.add(ballot.remove(Math.abs(rand)));
         }
         super.luckyAbstractMessages = list;
     }
--- src/main/java/com/everest/engineering/message/DefaultMessage.java
+++ src/main/java/com/everest/engineering/message/DefaultMessage.java
@@ -2,6 +2,8 @@ package com.everest.engineering.message;
 
 import com.everest.engineering.kingdom.AbstractKingdom;
 
+import java.util.Random;
+
 public class DefaultMessage extends AbstractMessage{
     public DefaultMessage(AbstractKingdom sendor, AbstractKingdom receiver) {
         super(sendor, receiver);
@@ -9,8 +11,10 @@ public class DefaultMessage extends AbstractMessage{
 
     @Override
     protected String getRandomMessage() {
+        Random random = new Random(); //ToDo: Too many instantiations.
+        int i = random.nextInt();
         //24 implies the total number of standard msgs in the table. This would allow to choose a message randomly.
-        int rand = (int)Math.random()%24;
-        return getMessageTable().get(rand);
+        int rand = (int)i%24;
+        return getMessageTable().get(Math.abs(rand));
     }
 }
