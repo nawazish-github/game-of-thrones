b7a6a0d6e9c57bc6df83e7a57b91b2eb4e254f74
Mohammad Nawazish Khan <md.nawazish.khan@gmail.com>
2019-02-25 17:13:18 +0530

    Instantiate Random instance only once and reuse it

--- src/main/java/com/everest/engineering/message/DefaultMessage.java
+++ src/main/java/com/everest/engineering/message/DefaultMessage.java
@@ -8,10 +8,10 @@ public class DefaultMessage extends AbstractMessage{
     public DefaultMessage(AbstractKingdom sendor, AbstractKingdom receiver) {
         super(sendor, receiver);
     }
+    private static Random random = new Random();
 
     @Override
     protected String getRandomMessage() {
-        Random random = new Random(); //ToDo: Too many instantiations.
         int i = random.nextInt();
         //24 implies the total number of standard msgs in the table. This would allow to choose a message randomly.
         int rand = (int)i%24;
